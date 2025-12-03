740c4daf138962449b8e42f7cf95af17771e1f96
Pallavi Kailas <119298061+pallavikailas@users.noreply.github.com>
2025-12-03 16:41:48 +0530

    Update ingest.py

--- src/ingest.py
+++ src/ingest.py
@@ -1,5 +1,5 @@
 from langchain_community.document_loaders import DirectoryLoader, PyPDFLoader
-from langchain.text_splitters import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from pathlib import Path
 from .config import settings
 
